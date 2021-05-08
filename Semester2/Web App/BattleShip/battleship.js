bcc47cb9a99a5d4ff7c2159648e1ba6cce38955b
unknown <abdelhakim@univ-tiaret.dz>
2021-05-08 04:59:34 +0100

    js 23

--- Semester2/Web App/BattleShip/battleship.js
+++ Semester2/Web App/BattleShip/battleship.js
@@ -122,7 +122,13 @@ function parseGuess(guess){
         var row = alphabet.indexOf(firstChar);
         var column = guess.charAt(1);
 
-       
+        if(isNaN(row) || isNaN(column)){
+            alert("Oops, that isn't on the board!");
+        } else if(row<0 || row > model.boardSize || column<0 || column>=model.boardSize){
+            alert("Oops, that's off the board!");
+        } else{
+            return row + column;
+        }
     }
     return null;
 }
