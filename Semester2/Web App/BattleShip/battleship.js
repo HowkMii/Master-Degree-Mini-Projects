77a200938dc6f5008b875386ea71246456e24cdb
unknown <abdelhakim@univ-tiaret.dz>
2021-05-08 04:47:16 +0100

    js 4

--- Semester2/Web App/BattleShip/battleship.js
+++ Semester2/Web App/BattleShip/battleship.js
@@ -10,5 +10,10 @@ var view = {
         var cell = document.getElementById(location);
         cell.setAttribute("class", "hit");
     },
+    displayMiss: function(location){
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};
 
 window.onload = init;
